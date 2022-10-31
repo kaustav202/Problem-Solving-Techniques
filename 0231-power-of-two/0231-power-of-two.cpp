703a28bdad03f4d5cb08a186469b7723c162344c
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2022-10-31 16:32:29 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (89.04%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0){
+            return false;
+        }
+        if (n == 1){
+            return true;
+        }else{
+            return n%2==0 && isPowerOfTwo(n/2);
+        }
+    }
+};
