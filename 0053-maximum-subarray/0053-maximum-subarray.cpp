b237100208a276a770c77df9f6ab320002359383
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2022-12-13 13:47:03 +0530

    Time: 136 ms (80.49%), Space: 67.7 MB (55.87%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int net_max = INT_MIN;
+
+        int running_sum = 0;
+        
+        for(int i=0; i<nums.size(); i++){
+            
+            int curr = running_sum >= 0 ? running_sum + nums[i] : nums[i];
+            running_sum = curr >= 0 ? curr : 0;
+            net_max = curr > net_max ? curr : net_max;   
+        }
+        
+        return net_max;
+        
+    }
+};
