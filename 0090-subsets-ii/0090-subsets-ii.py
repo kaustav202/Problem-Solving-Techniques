cb7e73d0b430ef439d1b15fde783367fcec48dab
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2024-04-18 07:07:21 +0530

    Time: 44 ms (27.15%), Space: 16.8 MB (50.51%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsetsWithDup(self, nums):
+        ret = []
+        self.dfs(sorted(nums), [], ret)
+        return ret
+    
+    def dfs(self, nums, path, ret):
+        ret.append(path)
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums[i+1:], path+[nums[i]], ret)
