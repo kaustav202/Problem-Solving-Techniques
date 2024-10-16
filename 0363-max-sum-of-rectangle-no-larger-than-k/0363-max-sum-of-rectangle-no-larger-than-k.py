32233375c9fbf79fa221072b5032fd3a1724fb06
Kaustav Ganguly <89788120+kaustav202@users.noreply.github.com>
2024-10-16 23:53:15 +0530

    Time: 2527 ms (22.07%), Space: 17.8 MB (44.83%) - LeetHub

---
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.py
@@ -0,0 +1,28 @@
+from sortedcontainers import SortedList
+class Solution:
+    def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
+        m, n = len(matrix), len(matrix[0])
+        ans = -math.inf
+        for r1 in range(m):
+            arr = [0] * n  # arr[i] is sum(matrix[r1][c]...matrix[r2][c])
+            for r2 in range(r1, m):
+                for c in range(n): arr[c] += matrix[r2][c]
+                ans = max(ans, self.maxSumSubAarray(arr, n, k))
+        return ans
+
+    def maxSumSubAarray(self, arr, n, k):
+        right = 0  # PrefixSum so far
+        seen = SortedList([0])
+        ans = -math.inf
+        for i in range(n):
+            right += arr[i]
+            left = self.ceiling(seen, right - k)  # right - left <= k -> left >= right - k
+            if left != None:
+                ans = max(ans, right - left)
+            seen.add(right)
+        return ans
+
+    def ceiling(self, sortedList, key):  # O(logN)
+        idx = sortedList.bisect_left(key)
+        if idx < len(sortedList): return sortedList[idx]
+        return None
